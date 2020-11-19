17245bfa537a0be5d5a7c0801eefb495b9424d97
Jeena <jeenathampi.23@gmail.com>
2020-11-18 20:46:03 -0800

    removed unused code

--- src/main/java/com/marketplace/usermanagement/models/Sale.java
+++ src/main/java/com/marketplace/usermanagement/models/Sale.java
@@ -2,8 +2,6 @@ package com.marketplace.usermanagement.models;
 
 import lombok.Data;
 
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
@@ -11,7 +9,6 @@ import java.util.List;
 @Data
 public class Sale {
     String orderId;
-    @OneToMany
     List<OrderDetails> orderDetailsDTO;
     BigDecimal totalPrice;
     BigDecimal taxAmount;
